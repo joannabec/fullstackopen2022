822be351c38a254aa9967711d986389e1673fdb9
joannabec <mariela.benitezc9@gmail.com>
2022-06-29 19:12:19 -0500

    exercises4.6-4.7

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -12,8 +12,44 @@ const favoriteBlog = (blogs) => {
   return ({ title: blog.title, author: blog.author, likes: blog.likes })
 }
 
+const filterMaxAuthor = (obj, prop) => {
+  let result = {}
+  Object.keys(obj).forEach((key, i) => {
+    if(i === 0) {
+      result.author = key
+      result[prop] = obj[key]
+    } else if (result[prop] < obj[key]) {
+      result.author = key
+      result[prop] = obj[key]
+    }
+  })
+
+  return result
+}
+const mostBlogs = (blogs) => {
+  let sumBlogs = {}
+  blogs.forEach(item => {
+    if(sumBlogs[item.author]) sumBlogs[item.author] += 1
+    else sumBlogs[item.author] = 1
+  })
+
+  return filterMaxAuthor(sumBlogs, 'blogs')
+}
+
+const mostlikes = (blogs) => {
+  let sumLikes = {}
+  blogs.forEach(item => {
+    if(sumLikes[item.author]) sumLikes[item.author] += item.likes
+    else sumLikes[item.author] = item.likes
+  })
+
+  return filterMaxAuthor(sumLikes, 'likes')
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostlikes
 }
