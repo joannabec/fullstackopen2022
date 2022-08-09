1a8c48ae9a8da5808c3e2fa43d00cbdb30bdb7c6
joannabec <mariela.benitezc9@gmail.com>
2022-08-09 17:39:54 -0500

    exercise7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -1,42 +1,6 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-
-  useEffect(() => {})
-
-  return country
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return <div>not found...</div>
-  }
-
-  return (
-    <div>
-      <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
-      <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
-    </div>
-  )  
-}
+import { useState } from 'react'
+import Country from './components/Country'
+import { useField, useCountry } from './hooks'
 
 const App = () => {
   const nameInput = useField('text')
